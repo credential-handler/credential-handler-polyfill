05ce796988590c4863dd0b396be10cec6d2a3307
Dmitri Zagidulin <dzagidulin@gmail.com>
2019-08-14 21:52:28 -0400

    Minimal wallet demo WIP.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ module.exports = {
   entry: {
     'credential-handler-polyfill': './index.js'
   },
+  devtool: 'source-map',
+  optimization: {
+    minimize: false
+  },
   output: {
     filename: '[name].min.js',
     library: 'credentialHandlerPolyfill',

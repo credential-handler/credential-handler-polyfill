1e513901149ae4b1903c5066c55d82cb3a9d3522
Dave Longley <dlongley@digitalbazaar.com>
2023-03-09 17:45:04 -0500

    Add `protocols` option to get/store.

--- WebCredential.js
+++ WebCredential.js
@@ -2,16 +2,19 @@
  * A WebCredential is a Credential that can be retrieved from or stored by a
  * "credential handler" that runs in a third party Web application.
  *
- * Copyright (c) 2017-2021 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2017-2023 Digital Bazaar, Inc. All rights reserved.
  */
 export class WebCredential {
-  constructor(dataType, data, {recommendedHandlerOrigins = []} = {}) {
+  constructor(dataType, data, {
+    recommendedHandlerOrigins = [],
+    protocols = {}
+  } = {}) {
     if(typeof dataType !== 'string') {
       throw new TypeError('"dataType" must be a string.');
     }
     this.type = 'web';
     this.dataType = dataType;
     this.data = data;
-    this.options = {recommendedHandlerOrigins};
+    this.options = {recommendedHandlerOrigins, protocols};
   }
 }

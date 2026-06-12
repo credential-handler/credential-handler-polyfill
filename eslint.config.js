2bd118bf191518194a33e84a33793b101a991b82
Dave Longley <dlongley@digitalbazaar.com>
2026-06-12 13:01:32 -0400

    Update file headers.
    Co-authored-by: Dave Longley <dlongley@digitalbazaar.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright (c) 2026 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2026 Digital Bazaar, Inc.
  */
 import config from '@digitalbazaar/eslint-config/browser-recommended';
 import globals from 'globals';
